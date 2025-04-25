933f378113b28d14b5b1d00d0fe314b9577e3bd3
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-25 21:28:49 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,10 +11,33 @@ class TodoController extends Controller
 {
     public function view(){
         //$todos = Todo::all();
-        $todos =Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-        return view("todo.view");
+        $todos =Todo::where('user_id', Auth::id())->orderBy('created_at', 'desc')->get();
+        // dd($todos);
+        return view("todo.view", compact('todos'));
     }
+
+    public function create(Request $request) {
+        return view('todo.create');
+    }
+
+    // public function edit(Request $request) {
+    //     return view('todo.');
+    // }
+
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'title' => 'required|string|max:255',
+        ]);   
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => Auth::id(),
+        ]);    
+        return redirect()->route('todo.view')->with('success', 'Todo created successfully.');
+    }
+
 
     // public function create(){
     
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,15 +1,61 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('User Index') }}
+            {{ __('User') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Halamana User Index!") }}
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">Id</th>
+                                <th scope="col" class="px-6 py-3">Nama</th>
+                                <th scope="col" class="hidden px-6 py-3 md:block">Email</th>
+                                <th scope="col" class="px-6 py-3">Todo</th>
+                                <th scope="col" class="px-6 py-3">Action</th>
+                            </tr>
+                        </thead>
+                        
+                        <tbody>
+                            @forelse ($users as $data)
+                                <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
+                                    <td scope="row" class="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
+                                        {{ $data->id }}
+                                    </td>
+                                    <td class="px-6 py-4">{{ $data->name }}</td>
+                                    <td class="hidden px-6 py-4 md:block">{{ $data->email }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <p>
+                                            {{ $data->todos->count() }}
+                                            <span>
+                                                <span class="text-green-600 dark:text-green-400">
+                                                    ({{ $data->todos->where('is_done', true)->count() }}
+                                                </span>/
+                                                <span class="text-blue-600 dark:text-blue-400">
+                                                    {{ $data->todos->where('is_done', false)->count() }})
+                                                </span>
+                                            </span>
+                                        </p>
+                                    </td>
+                                    <td class="px-6 py-4"></td>
+                                </tr>
+                            @empty
+                                <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
+                                    <td colspan="5" class="px-6 py-4 text-center text-gray-500 dark:text-gray-400">
+                                        No data available
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="px-6 py-5">
+                    {{ $users->links() }}
                 </div>
             </div>
         </div>
