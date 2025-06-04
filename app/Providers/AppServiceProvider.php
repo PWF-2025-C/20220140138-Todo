e48356380aa7a0002330c29800d54a488661b089
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-04 22:52:03 +0700

    api : auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+// use App\Models\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Str;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -19,11 +25,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    // 
+    
     public function boot(): void
     {
         Paginator::useTailwind();
         Gate::define('admin', function ($user){
             return $user->is_admin == true;
+        });
+        // Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+        Scramble::configure()->routes(function (Route $route){
+            return Str::startsWith($route->uri, 'api/');
         });
     }
 }
