7f0e696ce370f3613649d102f5a2821e3c26ea49
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-16 00:45:25 +0700

    api : logout, create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TodoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:api');
+// ->middleware('auth:sanctum');
+
 
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+// });
+Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+     Route::get('/todos/search', [TodoController::class, 'search']);
+    Route::apiResource('/todos', App\Http\Controllers\API\TodoController::class);
 });
+
